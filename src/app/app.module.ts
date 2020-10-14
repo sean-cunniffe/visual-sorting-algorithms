8270e14aa7738eeab29a0697f271916d25cabbc6
sean-cunniffe <sean.cunniffe927@gmail.com>
2020-10-14 18:58:38 +0100

    quicksort+gui update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BubbleSortComponent } from './components/bubble-sort/bubble-sort.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BubbleSortComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
